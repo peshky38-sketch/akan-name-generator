c31693df3fc5bd950a5c0b71816a3ebae5b05140
rachelkanyoro <peshky38@gmail.com>
2026-05-02 13:24:12 +0300

    Add form inputs for day, month, year and gender selection

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 document.getElementById("akanForm").addEventListener("submit", function(e) {
     e.preventDefault();
+    let day = parseInt(document.getElementById("day").value);
+    let month = parseInt(document.getElementById("month").value);
+    let year = parseInt(document.getElementById("year").value);
+    let gender = document.querySelector('input[name="gender"]:checked');
