c4f7471e51147811420c9b1fd1d125252b9d9f59
rachelkanyoro <peshky38@gmail.com>
2026-05-02 13:49:45 +0300

    Add project description to landing page

--- script.js
+++ script.js
@@ -43,4 +43,5 @@ document.getElementById("akanForm").addEventListener("submit", function(e) {
         `You were born on ${dayName}. Your Akan name is ${akanName}.`;
 
     // clear form (portfolio polish)  
- document.getElementById("akanForm")  
+ document.getElementById("akanForm").reset();
+});  
